c815308d23fe6420f20b988c511e33267b59f320
Crowdbotics <team@crowdbotics.com>
2024-04-11 19:06:35 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "philip-irvine-app-47474",
+  url: "https://philip-irvine-app-47474.botics.co",
+  api: "https://philip-irvine-app-47474.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
